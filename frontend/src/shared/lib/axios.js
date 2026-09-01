2e241016620c1ec3504bedc3f8df81aa3188db6d
Chscodes <124598664+Chscodes@users.noreply.github.com>
2026-09-01 16:02:02 +0800

    Move axios client to shared/lib

---
+++ frontend/src/shared/lib/axios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? "",
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message ||
+      error.message ||
+      "Something went wrong";
+    return Promise.reject(new Error(message));
+  },
+);
+
+export default api;
